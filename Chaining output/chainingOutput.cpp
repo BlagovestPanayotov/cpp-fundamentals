1d1f168e96fc9c81ff893ececf2acb1769b9494b
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2024-05-04 15:02:29 +0100

    Chaining output

---
+++ Chaining output/chainingOutput.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    int y = 20;
+    // std::cout << "x = ";
+    // std::cout << x;
+
+    // std::cout << "x = " << x;
+
+    // std::cout << "x = " << x << std::endl;
+    // std::cout << "y = " << y;
+
+    // std::cout << "x = " << x << std::endl
+    //           << "y = " << y;
+
+    cout << "x = " << x << endl
+         << "y = " << y;
+    return 0;
+}
